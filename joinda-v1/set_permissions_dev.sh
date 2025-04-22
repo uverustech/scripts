d4ad673b9f67312cd9000e4712bb324d25df17c9
Deji <thedejijoseph@gmail.com>
2025-04-22 15:11:15 +0100

    add permissions mgt scripts

---
+++ joinda-v1/set_permissions_dev.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Script: set_permissions_dev.sh
+# Description: Loose 777 permission setup for development
+
+WEB_ROOT="/www/wwwroot/dpanza.joinda.net"
+
+# Create /xml directory if it doesn't exist
+if [ ! -d "$WEB_ROOT/xml" ]; then
+  echo "Creating missing /xml directory..."
+  mkdir "$WEB_ROOT/xml"
+fi
+
+# Set recursive 777 permissions for dev
+echo "Setting 777 permissions recursively..."
+chmod -R 777 "$WEB_ROOT/upload"
+chmod -R 777 "$WEB_ROOT/xml"
+chmod -R 777 "$WEB_ROOT/cache"
+chmod -R 777 "$WEB_ROOT/nodejs/models"
+
+# Done
+echo "Development permissions applied to writable directories."
---
+++ joinda-v1/set_permissions_secure.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Script: set_permissions_secure.sh
+# Description: Secure permission setup for production
+
+WEB_ROOT="/www/wwwroot/dpanza.joinda.net"
+APACHE_USER="www-data"
+
+# Set ownership to Apache user
+echo "Setting ownership to $APACHE_USER..."
+chown -R $APACHE_USER:$APACHE_USER "$WEB_ROOT"
+
+# Set folder permissions to 775
+find "$WEB_ROOT" -type d -exec chmod 775 {} \;
+
+# Set file permissions to 664
+find "$WEB_ROOT" -type f -exec chmod 664 {} \;
+
+# Done
+echo "Secure permissions applied to $WEB_ROOT."
